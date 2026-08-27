62a8da60bffe76a45f4633b62578a0d1333dba69
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-27 17:56:05 +0530

    14 sloved

---
+++ level1/14questions.py
@@ -0,0 +1,15 @@
+#Question: Get a three-digit number from user and print the reverse of the number.#
+
+rev = 0 
+while True:
+    n = int(input())
+    if n >1000 or n<100:
+        print("Enter 3 digit number")
+        continue
+    break
+while n >0:
+    c = n %10
+    rev  = rev *10 +c
+    n = n//10
+else:
+    print(rev)
