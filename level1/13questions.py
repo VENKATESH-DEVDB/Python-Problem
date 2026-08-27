7993ea74cbcc1e3ba47079e7b9b29cd3bfcee0d7
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-27 17:53:53 +0530

    13 sloved

---
+++ level1/13questions.py
@@ -0,0 +1,14 @@
+#Question: Get a two-digit number from user and print the reverse of the number.#
+rev = 0 
+while True:
+    n = int(input())
+    if n >100 or n<10:
+        print("Enter 2 digit number")
+        continue
+    break
+while n >0:
+    c = n %10
+    rev  = rev *10 +c
+    n = n//10
+else:
+    print(rev)
