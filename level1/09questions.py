d9f09b796a65f4f976ce08b8336efd318b4f25ac
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-27 09:26:49 +0530

    09 sloved

---
+++ level1/09questions.py
@@ -0,0 +1,16 @@
+#Question: Get a three-digit number from user and print the hundred's digit.#
+count = 0 
+while True  :
+    
+    n = int(input())
+    if n > 1000 or n <100:
+        print( " enter a 3 digite number")
+        continue
+    break 
+while n >0:
+    a = n %10 
+    count = count +1 
+    if count == 3 :
+        print(a)
+        break 
+    n = n//10
