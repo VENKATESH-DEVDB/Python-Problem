9c7a3ac0361a8ecd2b11733c515f0f222debc097
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-27 09:22:26 +0530

    sloved

--- level1/06questions.py
+++ level1/06questions.py
@@ -1,3 +1,9 @@
 #Question: Get a two-digit number from user and print the one's digit#
-n = int(input())
-print(int(n%10))
+while True  :
+    
+    n = int(input())
+    if n > 100 or n <10:
+        print( " enter a 2 digite number")
+        continue
+    break 
+print ( n %10)
--- level1/07questions.py
+++ level1/07questions.py
@@ -1,10 +1,17 @@
     #Question: Get a two-digit number from user and print the ten's digit.#
-n = int(input())
 count = 0 
-while n >0 :
-    a = n %10
+while True  :
+    
+    n = int(input())
+    if n > 100 or n <10:
+        print( " enter a 2 digite number")
+        continue
+    break 
+while n >0:
+    a = n %10 
     count = count +1 
     if count == 2 :
         print(a)
         break 
-    n = n //10 
+    n = n//10
+ 
