d58486a9d9f5d5717e418e6763afc7043a3822c8
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-27 17:46:29 +0530

    11 sloved

---
+++ level1/11questions.py
@@ -0,0 +1,16 @@
+#Question: Get a two-digit number from user and print sum the digits.#
+two_sum = 0 
+while True  :
+    
+    n = int(input())
+    if n > 100 or n <10:
+        print( " enter a 2 digite number")
+        continue
+    break 
+while n>0:
+    c= n%10
+    two_sum = two_sum +c
+    n=n//10
+else :
+    print(two_sum)
+    
