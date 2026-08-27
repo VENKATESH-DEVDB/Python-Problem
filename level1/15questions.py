f422abbfd4b17bdd2ab44f34933c93339e6e87a7
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-27 18:13:01 +0530

    15 sloved

---
+++ level1/15questions.py
@@ -0,0 +1,18 @@
+#Question: Get a four-digit number from user and only reverse the first two digits of the number,
+#then print the number.
+
+
+rev = 0 
+while True:
+    n = int(input())
+    if n >10000 or n<1000:
+        print("Enter 4 digit number")
+        continue
+    break
+while n >100:
+    c = n %10
+    rev  = rev *10 +c
+    n = n//10
+else:
+    rev = n*100 + rev
+    print(rev)
