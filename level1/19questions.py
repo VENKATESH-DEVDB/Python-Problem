582beb7eb0d1712bd44b57fab80ea257ca3ea3e1
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-28 19:33:31 +0530

    19 sloved

---
+++ level1/19questions.py
@@ -0,0 +1,10 @@
+#Question: Get a three-digit number from user and make the one's digit as 2, then print it
+while True:
+    n = int(input())
+    if n >1000 or n<=100:
+        print("enter a 3 digit number")
+        continue
+    break 
+if n > 0:
+    c = n //10
+    print((c*10) + 2 )
