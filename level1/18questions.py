6f28ded4b07d93b807a023f9df67a85567ebb4f6
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-28 19:29:56 +0530

    18 sloved

---
+++ level1/18questions.py
@@ -0,0 +1,10 @@
+#Question: Get a two-digit number from user and make the ten's digit 1, then print it.
+while True :
+    n = int(input())
+    if n>100 or n<=10:
+        print("enter a 2 digit number")
+        continue
+    break 
+if n > 0 :
+    c = n%10
+    print(10+c)
