8d31950d8f48dfaadaa440e653bf4353a79141d2
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-28 21:17:09 +0530

    26 sloved

---
+++ level1/26questions.py
@@ -0,0 +1,15 @@
+#Question: Get a two-digit number from user. If the sum of the digits is 10 then print "Success",
+#otherwise print "Failure".
+
+while True:
+    n = int(input())
+    if n >100 or n<=10:
+        print("Enter 2 digit number")
+        continue
+    break
+
+total = (n%10)+(n//10)
+if total == 10:
+    print("Success") 
+else:
+    print("Failure")
