86ee53961c9d9ada616f45dcacc9950521878802
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-28 19:36:47 +0530

    20 sloved

---
+++ level1/20questions.py
@@ -0,0 +1,12 @@
+#Question: Get a three-digit number from user and make the ten's digit as 0, then print i
+while True:
+    n = int(input())
+    if n >1000 or n<=100:
+        print("enter a 3 digit number")
+        continue
+    break 
+if n > 0:
+    c = n //100
+    c = c*100
+    last_digit=n %10
+    print(c+last_digit) 
