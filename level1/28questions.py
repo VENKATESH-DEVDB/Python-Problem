bc70578b84a8624ed323e115c49e99d4da22beb7
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-08-28 21:27:55 +0530

    28 sloved

---
+++ level1/28questions.py
@@ -0,0 +1,14 @@
+#Question: Get a three-digit number from user. If the sum of the one's digit and hundred's digit is
+#less than 10, then print "Success", otherwise print "Failure".
+
+while True:
+    n = int(input())
+    if n >1000 and n<=100:
+        print("Enter 3 digit number")
+        continue
+    break
+total = (n % 10)+(n//100)
+if total < 10:
+    print("Success") 
+else:
+    print("Failure")
