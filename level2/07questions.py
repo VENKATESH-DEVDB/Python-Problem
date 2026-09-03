d519c07dce8aadce29d90a97bd17b04bd8e93ba7
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-03 19:38:08 +0530

    07 sloved

--- level2/07questions.py
+++ level2/07questions.py
@@ -1,8 +1,7 @@
 #Question: Write a loop program to print the two-digit odd numbers whose sum of digits
 #is 7.
-n = int(input())
 
-for i in range(16,n):
+for i in range(16,71):
     if i %2 != 0:
         if i//10 + i%10 == 7:
             print(i)
