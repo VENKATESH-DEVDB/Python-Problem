cd0ac2efad980dbafb1fe3ce13fdb1ca57cb9e0b
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-03 19:51:38 +0530

    10 sloved

---
+++ level2/10questions.py
@@ -0,0 +1,7 @@
+#Question: Write a loop program to print the sum of two-digit odd numbers whose ten's digit is 7.
+total = 0 
+for i in range(71 , 80, 2):
+        
+        total = total + i 
+
+print(total)
