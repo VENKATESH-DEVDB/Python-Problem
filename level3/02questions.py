e3785d44d329362062c06a93fa47a8d3e5473b7c
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-06 20:42:09 +0530

    02 sloved

---
+++ level3/02questions.py
@@ -0,0 +1,7 @@
+#Question: Get a number from user and subtract 5 from that number and print the result. 
+# Write your code inside the function.
+
+n = int(input())
+def sub (a):
+    return a-5
+print(sub(n))
