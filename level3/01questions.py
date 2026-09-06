b29791c3352702419ff5837c14b96e29bdd50af8
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-06 20:40:14 +0530

    01 sloved

---
+++ level3/01questions.py
@@ -0,0 +1,6 @@
+#Question: Get a number from user and add 2 to that number and print the result. Write your code inside the function.
+
+n = int(input())
+def add (a):
+    return a+2
+print(add(n))
