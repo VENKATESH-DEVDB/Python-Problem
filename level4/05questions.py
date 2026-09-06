632f72ea2d409a82d3ea7c6da3e11edc51a09d92
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-06 22:17:05 +0530

    05 solved

---
+++ level4/05questions.py
@@ -0,0 +1,11 @@
+#Question: Get a three-digit number from the user and print the digit in the hundred's
+#position.
+
+while True:
+    n = int(input())
+    if n >1000 or n<99:
+        print("Enter a three digit number")
+        continue
+    break
+
+print((n//100))
