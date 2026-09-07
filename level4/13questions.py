a22f5735067945e0b3d97577bfd7296a0392bcbd
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-07 14:19:39 +0530

    13 solved

---
+++ level4/13questions.py
@@ -0,0 +1,10 @@
+#Question: Get a number from the user and print the sum of all digits
+
+n = int(input())
+total  = 0 
+while n > 0:
+    c = n %10 
+    n = n//10
+    total += c 
+else : 
+    print(total)
