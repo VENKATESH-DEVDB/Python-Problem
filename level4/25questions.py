b82facfd6991ffc17341c23f8e90ef34e5668a12
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-07 18:56:40 +0530

    25 solved

---
+++ level4/25questions.py
@@ -0,0 +1,12 @@
+#Question: Write a program to print the sum of all three-digit prime numbers.
+
+
+total = 0
+for i in range(101,1000,2 ):
+    for j in range(2,int(i**0.5)+1):
+        if i%j == 0:
+            break 
+
+    else:
+            total +=i
+print(total)
