b5c7ebfe8391cd746513c28a0b8ee1f795582c77
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-07 20:48:03 +0530

    27 solved

--- level4/27questions.py
+++ level4/27questions.py
@@ -1,9 +1,7 @@
 #Question: Print the largest three-digit prime number.
-curentprim = 0 
-prevprime =97 
+
 for i in range(999,99,-1):
     for j in range(2,int(i**0.5)+1):
-        curentprim = i 
         if i%j == 0:
             
             break 
