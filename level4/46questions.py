3d6fb29afaa4329dd633621602e9cd49c2e19f68
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-11 21:11:52 +0530

    46 solved

---
+++ level4/46questions.py
@@ -0,0 +1,13 @@
+#Question: Get a number string up to 50 digits and convert it into an integer array.
+import Numpy as np 
+Max = 50 
+while True:
+    n = input()
+    if len(n)> Max:
+        print (" enter digit only up to 50 digit ")
+        continue
+    break
+arr = np.array([int(i) for i in n])
+
+
+print(arr)
