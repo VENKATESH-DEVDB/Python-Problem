d8c2b8706af177707d9d621ac51ea394b25edec1
VENKATESH-DEVDB <venkatesh.devdb@gmail.com~>
2026-09-11 21:46:58 +0530

    49 solved

---
+++ level4/49questions.py
@@ -0,0 +1,12 @@
+#Question: Write a function to convert an integer array into a character array and print it.
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6,7])
+
+string = []
+for i in arr :
+    string.append(str(i))
+else:
+    for i in string:
+        print(i)
